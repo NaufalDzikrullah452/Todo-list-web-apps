50a4e737f0f82d4ac871c3e20bcb310aa181fb09
muhammadarifsp <85152766+muhammadarifsp@users.noreply.github.com>
2021-08-29 19:03:23 +0800

    login dan register
    tpi alertnya pke session kok stack di refresh ga ilang, di v_sign_in baris 42-43

--- application/controllers/Sign_in.php
+++ application/controllers/Sign_in.php
@@ -1,10 +1,51 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Sign_in extends CI_Controller {
-
+class Sign_in extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
 	public function index()
 	{
-		$this->load->view('layout/v_sign_in');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('pass', 'Password', 'trim|required');
+		if ($this->form_validation->run() == false) {
+			# code...
+			$this->load->view('layout/v_sign_in');
+		} else {
+			// validasi success
+			$this->_login();
+		}
+	}
+
+	private function _login()
+	{
+		$email = $this->input->post('email');
+		$pass = md5($this->input->post('pass'));
+		$user = $this->db->get_where('tbl_user', ['user_email' => $email])->row_array();
+
+		if ($user) {
+			if ($pass == $user['user_password']) {
+				// if user exist
+				$data = [
+					'user_picture' => $user['user_picture'],
+					'user_username' => $user['user_username'],
+					'user_email' => $user['user_email']
+				];
+				$this->session->set_userdata($data);
+				redirect('welcome_message.php');
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+				Your password wrong! </div>');
+				redirect('Sign_in');
+			}
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+			Your email not registered!</div>');
+			redirect('Sign_in');
+		}
 	}
 }
--- application/views/layout/v_sign_in.php
+++ application/views/layout/v_sign_in.php
@@ -1,54 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Sign In Your Todo</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?php echo base_url('assets/auth/images/icons/favicon.ico');?>"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="<?php echo base_url('assets/auth/images/icons/favicon.ico'); ?>" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('vendor') ?>bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('fonts') ?>font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('fonts') ?>Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('vendor') ?>animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('vendor') ?>css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('vendor') ?>animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('vendor') ?>select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('vendor') ?>daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= config_item('css_auth') ?>util.css">
 	<link rel="stylesheet" type="text/css" href="<?= config_item('css_auth') ?>main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body style="background-color: #666666;">
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="post" action="<?= base_url('sign_in/index') ?>">
 					<span class="login100-form-title p-b-43">
-						Hi, Welcome Back
+						Login to continue
 					</span>
-					
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email">
+
+					<?php if ($this->session->flashdata('message')) {
+						echo $this->session->flashdata('message');
+					} ?>
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" id="email" name="email">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Email</span>
 					</div>
-					
-					
+					<?= form_error('email', '<small class="text-danger pl-2">', '</small>') ?>
+
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass">
-						<span class="focus-input100"></span>
+						<input class="input100" type="password" id="pass" name="pass">
+						<span class=" focus-input100"></span>
 						<span class="label-input100">Password</span>
 					</div>
+					<?= form_error('pass', '<small class="text-danger pl-2">', '</small>') ?>
 
 					<div class="flex-sb-m w-full p-t-3 p-b-32">
 						<div class="contact100-form-checkbox">
@@ -64,61 +70,58 @@
 							</a>
 						</div>
 					</div>
-			
+
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn" type="submit">
 							Login
 						</button>
 					</div>
 
-					<div><br>
-					Don't have an account?
-							<a href="#" class="txt3"> &nbsp;Sign Up</a>
-						</div>
-						<br>
-					<hr>
-					<div class="text-center p-t-30 p-b-20">
+					<div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
-							or sign in using
+							or sign up using
 						</span>
 					</div>
 
 					<div class="login100-form-social flex-c-m">
-						
+						<a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+							<i class="fa fa-facebook-f" aria-hidden="true"></i>
+						</a>
+
 						<a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
 							<i class="fa fa-google" aria-hidden="true"></i>
 						</a>
-
 					</div>
 				</form>
 
-				<div class="login100-more" style="background-image: url('<?php echo base_url('assets/auth/images/bg_icon.jpg');?>">
+				<div class="login100-more" style="background-image: url('<?php echo base_url('assets/auth/images/bg-01.jpg'); ?>">
 				</div>
 			</div>
 		</div>
 	</div>
-	
-	
 
-	
-	
-<!--===============================================================================================-->
+
+
+
+
+	<!--===============================================================================================-->
 	<script src="<?= config_item('vendor') ?>jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?= config_item('vendor') ?>animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?= config_item('vendor') ?>bootstrap/js/popper.js"></script>
 	<script src="<?= config_item('vendor') ?>bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?= config_item('vendor') ?>select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?= config_item('vendor') ?>daterangepicker/moment.min.js"></script>
 	<script src="<?= config_item('vendor') ?>daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?= config_item('vendor') ?>countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="<?= config_item('js_auth') ?>main.js"></script>
 
 </body>
+
 </html>
